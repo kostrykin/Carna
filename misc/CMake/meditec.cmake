81f997660bcaba1f597dd6ba7b5b016e1326a642
Leonid Kostrykin <void@evoid.de>
2014-12-12 11:03:54 +0100

    added FindEigen3.cmake and FindGDCM.cmake, refactored FindTRTK.cmake, s.t. it sticks closer to the idea of CMake

--- misc/CMake/meditec.cmake
+++ misc/CMake/meditec.cmake
@@ -3,27 +3,24 @@
 # ------------------------------------------
 # The script does nothing unless the
 # environmental variable MEDITEC_LIBS is set
+#-------------------------------------------
+# Leonid Kostrykin, 12.12.2014
 ############################################
 
 if( DEFINED ENV{MEDITEC_LIBS} )
-
-	# set where Find<ModuleName>.cmake scripts are located
-	list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/misc/MediTEC)
-
 	
-	# make sure GLEW includes will be found
+	# make sure GLEW headers will be found
 	list(APPEND CMAKE_INCLUDE_PATH "$ENV{MEDITEC_LIBS}/GLEW/1.7.0/include")
 	
 	# find GLEW libraries
 	list(APPEND CMAKE_LIBRARY_PATH "$ENV{MEDITEC_LIBS}/GLEW/1.7.0/bin")
 	find_library(GLEW_LIBRARY NAMES glew32-1.7.0 glew32s-1.7.0 PATH_SUFFIXES lib64)
 	
-	
-	# make sure TRTK includes will be found
-	list(APPEND CMAKE_INCLUDE_PATH "$ENV{MEDITEC_LIBS}/TRTK/0.13.1/include")
-	
-	# find TRTK libraries
-	list(APPEND CMAKE_LIBRARY_PATH "$ENV{MEDITEC_LIBS}/TRTK/0.13.1/bin")
-	find_library(TRTK_LIBRARY NAMES TRTK-0.13.1)
+	# find Eigen headers
+	find_path(	EIGEN3_INCLUDE_DIR
+				Eigen/Core
+				PATHS ENV MEDITEC_LIBS
+				PATH_SUFFIXES "Eigen/3.0.5/include"
+				DOC "Eigen3 headers")
 
 endif( DEFINED ENV{MEDITEC_LIBS} )
