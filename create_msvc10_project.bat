4f25482c4344fa1b9d2ba0416c4286d9cee922f3
Leonid Kostrykin <void@evoid.de>
2014-12-12 16:49:32 +0100

    refactored INSTALL routines

--- create_msvc10_project.bat
+++ create_msvc10_project.bat
@@ -6,6 +6,6 @@ mkdir build
 mkdir build\VisualStudio2010
 
 cd build\VisualStudio2010
-cmake -G"Visual Studio 10" -DBUILD_TEST=ON ..\..
+cmake -G"Visual Studio 10" -DCMAKE_INSTALL_PREFIX="%MEDITEC_LIBS%" -DBUILD_TEST=ON ..\..
 
 cd ..\..
--- win32_msvc10_build.bat
+++ win32_msvc10_build.bat
@@ -19,16 +19,16 @@ mkdir build\nmake_release
 mkdir build\VisualStudio2010
 
 cd build\nmake_debug
-cmake -G"NMake Makefiles" %1 %2 ..\..
+cmake -G"NMake Makefiles" %1 %2 -DCMAKE_INSTALL_PREFIX="%MEDITEC_LIBS%" ..\..
 nmake
 nmake install
 
 cd ..\nmake_release
-cmake -G"NMake Makefiles" %1 %2 -DBUILD_DOC=OFF -DCMAKE_BUILD_TYPE=Release ..\..
+cmake -G"NMake Makefiles" %1 %2 -DCMAKE_INSTALL_PREFIX="%MEDITEC_LIBS%" -DBUILD_DOC=OFF -DCMAKE_BUILD_TYPE=Release ..\..
 nmake
 nmake install
 
 cd ..\VisualStudio2010
-cmake -G"Visual Studio 10" -DBUILD_TEST=ON ..\..
+cmake -G"Visual Studio 10" -DCMAKE_INSTALL_PREFIX="%MEDITEC_LIBS%" -DBUILD_TEST=ON ..\..
 
 cd ..\..
