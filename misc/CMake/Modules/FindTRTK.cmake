04a8804d258ea9edea39cdcccb55918775e38f1b
Leonid Kostrykin <void@evoid.de>
2014-12-12 11:35:48 +0100

    Fixed CMakeLists.txt and FindTRTK.cmake s.t. project can be built now. GDCM support not finished yet.

--- misc/CMake/Modules/FindTRTK.cmake
+++ misc/CMake/Modules/FindTRTK.cmake
@@ -11,7 +11,7 @@
 #-------------------------------------------
 # The following variables are set:
 #	TRTK_INCLUDE_DIR	points to headers
-#	TRTK_LIBRARY		points to lib file
+#	TRTK_LIBRARIES		points to lib files
 #	TRTK_FOUND			indicates success
 #-------------------------------------------
 # Leonid Kostrykin, 12.12.2014
@@ -21,15 +21,30 @@ find_path(	TRTK_INCLUDE_DIR
 			TRTK/Version.hpp
 			PATHS ENV MEDITEC_LIBS
 			PATH_SUFFIXES "TRTK/${TRTK_FIND_VERSION}/include"
-			DOC "TRTK headers")
+			DOC "TRTK headers" )
 
-find_library(	TRTK_LIBRARY
+find_library(	TRTK_LIBRARY_RELEASE
 				NAMES "TRTK-${TRTK_FIND_VERSION}" TRTK
 				PATHS ENV MEDITEC_LIBS
 				PATH_SUFFIXES "TRTK/${TRTK_FIND_VERSION}/bin"
-				DOC "TRTK library")
+				DOC "TRTK library (Release)" )
+
+find_library(	TRTK_LIBRARY_DEBUG
+				NAMES	"TRTK-${TRTK_FIND_VERSION}d"
+						"TRTK-${TRTK_FIND_VERSION}-debug"
+						TRTKd
+						TRTK-debug
+				PATHS ENV MEDITEC_LIBS
+				PATH_SUFFIXES "TRTK/${TRTK_FIND_VERSION}/bin"
+				DOC "TRTK library (Debug)" )
 
 # handles REQUIRED, QUIET and version-related
 # arguments and also sets the _FOUND variable
-find_package_handle_standard_args(TRTK
-	REQUIRED_VARS TRTK_INCLUDE_DIR TRTK_LIBRARY)
+find_package_handle_standard_args( TRTK
+	REQUIRED_VARS TRTK_INCLUDE_DIR TRTK_LIBRARY_RELEASE TRTK_LIBRARY_DEBUG )
+
+if( DEFINED TRTK_LIBRARY_RELEASE AND DEFINED TRTK_LIBRARY_DEBUG )
+	set( TRTK_LIBRARIES
+			optimized	${TRTK_LIBRARY_RELEASE}
+			debug		${TRTK_LIBRARY_DEBUG} )
+endif()
