839d7aba3be267aa154e6b936f9295a0846f28df
Leonid Kostrykin <void@evoid.de>
2014-12-12 02:09:37 +0100

    wip on CMakeLists.txt: TRTK support added

--- misc/MediTEC/meditec.cmake
+++ misc/MediTEC/meditec.cmake
@@ -7,11 +7,23 @@
 
 if( DEFINED ENV{MEDITEC_LIBS} )
 
+	# set where Find<ModuleName>.cmake scripts are located
+	list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/misc/MediTEC)
+
+	
 	# make sure GLEW includes will be found
 	list(APPEND CMAKE_INCLUDE_PATH "$ENV{MEDITEC_LIBS}/GLEW/1.7.0/include")
 	
 	# find GLEW libraries
 	list(APPEND CMAKE_LIBRARY_PATH "$ENV{MEDITEC_LIBS}/GLEW/1.7.0/bin")
 	find_library(GLEW_LIBRARY NAMES glew32-1.7.0 glew32s-1.7.0 PATH_SUFFIXES lib64)
+	
+	
+	# make sure TRTK includes will be found
+	list(APPEND CMAKE_INCLUDE_PATH "$ENV{MEDITEC_LIBS}/TRTK/0.13.1/include")
+	
+	# find TRTK libraries
+	list(APPEND CMAKE_LIBRARY_PATH "$ENV{MEDITEC_LIBS}/TRTK/0.13.1/bin")
+	find_library(TRTK_LIBRARY NAMES TRTK-0.13.1)
 
 endif( DEFINED ENV{MEDITEC_LIBS} )
