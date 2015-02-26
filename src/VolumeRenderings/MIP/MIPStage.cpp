6b41eeea4e41edf2ed2d7ddf51d4cc5f5353d748
Leonid Kostrykin <void@evoid.de>
2015-02-26 22:26:27 +0100

    fixed viewport-treatment in MIPStage::renderPass

--- src/VolumeRenderings/MIP/MIPStage.cpp
+++ src/VolumeRenderings/MIP/MIPStage.cpp
@@ -156,7 +156,7 @@ void MIPStage::renderPass
 
             glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-            RayMarchingStage::renderPass( vt, rt, vp );
+            RayMarchingStage::renderPass( vt, rt, framebufferViewport );
 
             framebufferViewport.done();
         );
