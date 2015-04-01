d6013f90dbd16d1494e6113e493b7bab1c3275f9
Leonid Kostrykin <void@evoid.de>
2015-04-01 13:44:55 +0200

    Added exemplary images to CompositionStage documentation.

--- include/Carna/presets/CompositionStage.h
+++ include/Carna/presets/CompositionStage.h
@@ -35,11 +35,8 @@ namespace presets
   * renderings either by putting them next to each other or through row-wise
   * interleaving.
   *
-  * \todo
-  * Finish this.
-  *
   * \author Leonid Kostrykin
-  * \date   22.2.15 - 11.3.15
+  * \date   22.2.15 - 31.3.15
   */
 class CARNA_LIB CompositionStage : public base::RenderStage
 {
@@ -54,8 +51,15 @@ public:
       */
     enum CompositionMode
     {
-        interleave, ///< Combines renderings through row-wise interleaving.
-        aside       ///< Combines renderings by putting them next to each other.
+        /** Combines renderings through row-wise interleaving.
+          * \image html ParallaxStageIntegrationTest/interleaved.png
+          */
+        interleave,
+
+        /** Combines renderings by putting them next to each other.
+          * \image html ParallaxStageIntegrationTest/aside.png
+          */
+        aside
     };
 
     /** \brief
