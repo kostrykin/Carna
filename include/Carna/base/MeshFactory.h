d8dc06269c6af134bebe638a598212efe4e0b62b
Tim <tschroeder@hia.rwth-aachen.de>
2015-07-23 11:35:33 +0200

    added MeshFactory:createFromSTL(...) method

--- include/Carna/base/MeshFactory.h
+++ include/Carna/base/MeshFactory.h
@@ -18,6 +18,13 @@
 #include <Carna/base/ManagedMesh.h>
 #include <Carna/base/Vertex.h>
 #include <Carna/base/math.h>
+#include <cstdint>
+#include <unordered_set>
+#include <functional>
+#include <memory>
+#include <istream>
+#include <ios>
+#include <string>
 
 /** \file   MeshFactory.h
   * \brief  Defines \ref Carna::base::MeshFactory.
@@ -68,6 +75,15 @@ public:
       * Creates mesh that consists of a single point.
       */
     static ManagedMesh< VertexType, uint8_t >& createPoint();
+
+	/** \brief
+	  * Creates mesh from a stl-file.
+	  */
+	inline static ManagedMesh< VertexType, uint32_t >& createFromSTL( const std::string& path );
+
+	/** \overload
+	 */
+	static ManagedMesh< VertexType, uint32_t >& createFromSTL( std::istream& stlStream );
 
 }; // MeshFactory
 
@@ -155,6 +171,109 @@ ManagedMesh< VertexType, uint8_t >& MeshFactory< VertexType >::createPoint()
     Index index = 0;
 
     return MeshInstance::create( IndexBufferBase::PRIMITIVE_TYPE_POINTS, &vertex, 1, &index, 1 );
+}
+
+template< typename VertexType >
+inline ManagedMesh< VertexType, uint32_t >& MeshFactory< VertexType >::createFromSTL(const std::string& path)
+{
+	return createFromSTL( std::fstream(path, std::ios::in | std::ios::binary) );
+}
+
+template< typename VertexType >
+ManagedMesh< VertexType, uint32_t >& MeshFactory< VertexType >::createFromSTL( std::istream& stlStream )
+{
+	auto origExceptMask = stlStream.exceptions();
+	stlStream.exceptions(std::istream::failbit | std::istream::badbit | std::istream::eofbit);
+
+	uint32_t amountTriangles;
+
+	//discard first 80 bytes
+	stlStream.seekg(stlStream.beg + 80);
+
+	//read amount of triangles (32bit-uint)
+	stlStream.read(reinterpret_cast<char*>(&amountTriangles), 4);
+	
+	typedef ManagedMesh< VertexType, uint32_t > MeshInstance;
+	typedef typename MeshInstance::Vertex Vertex;
+	typedef typename MeshInstance::Index  Index;
+	typedef typename std::pair< Vertex, std::size_t > VertexIndexPair;
+
+	std::size_t indicesCount = amountTriangles * 3;
+	std::unique_ptr<Index[]>  indices (new Index  [indicesCount]);
+	std::unique_ptr<Vertex[]> vertices(new Vertex [indicesCount]);	//indicesCount equals the maximum possible verticesCount
+	
+	std::function<std::size_t(const VertexIndexPair&)> vertexHash = //try auto instead
+	[](const VertexIndexPair& vertAndIndex) -> std::size_t
+	{
+		//creates combined hash value of the 3 public members "x","y","z" of a Vertex object; ignores the second member of pair
+		//This hash-combine-technique is taken from : http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3876.pdf (Page 2)
+		std::size_t hashVal = std::hash<decltype(vertAndIndex.first.x)>()(vertAndIndex.first.x);
+		hashVal ^= std::hash<decltype(vertAndIndex.first.y)>()(vertAndIndex.first.y) + 0x9e3779b9 + (hashVal << 6) + (hashVal >> 2);
+		hashVal ^= std::hash<decltype(vertAndIndex.first.z)> ()(vertAndIndex.first.z) + 0x9e3779b9 + (hashVal << 6) + (hashVal >> 2);
+		//is this suited for systems with size_t defined as integer values with more than 32 bits? (cause 0x9e3779b9 has 32 bits)... what about less?
+		return hashVal;
+	};
+
+	std::function<std::size_t(const VertexIndexPair&, const VertexIndexPair&)> vertexComp = //try auto instead
+	[](const VertexIndexPair& vertAndIndex1, const VertexIndexPair& vertAndIndex2) -> bool
+	{
+		//checks wether the members "x", "y", "z" of two Vertex objects are equal
+		return ((vertAndIndex1.first.x) == (vertAndIndex2.first.x)) && ((vertAndIndex1.first.y) == (vertAndIndex2.first.y)) && ((vertAndIndex1.first.z) == (vertAndIndex2.first.z));
+	};
+
+	std::unordered_set< VertexIndexPair, decltype(vertexHash), decltype(vertexComp) > vertsWithArrIndices(amountTriangles * 3, vertexHash, vertexComp);
+
+	std::size_t indicesItCount = 0;
+	std::size_t verticesItCount = 0;
+
+	//read triangles
+	for (unsigned int i = 0; i < amountTriangles; i++)
+	{
+		//discard normal
+		stlStream.seekg(12, std::ios_base::cur);
+		
+		//read vertices
+		for (unsigned int j = 0; j < 3; j++)    //read 3 vertices
+		{
+			float x, y, z;
+			
+			//read 1 vertex
+			stlStream.read(reinterpret_cast<char*>(&x), 4);
+			stlStream.read(reinterpret_cast<char*>(&y), 4);
+			stlStream.read(reinterpret_cast<char*>(&z), 4);
+
+			//std::pair< std::unordered_set< Vertex, decltype(vertWithIndicesHash) >::iterator, bool> emplRes = vertsWithArrIndices.emplace(std::piecewise_construct, std::forward_as_tuple(x, y, z), std::forward_as_tuple(VertCnt++));
+			Vertex vert;
+			vert.x = x;
+			vert.y = y;
+			vert.z = z;
+																				//!!vert is invalidated after here!!!
+			auto emplRes = vertsWithArrIndices.emplace(std::piecewise_construct, std::forward_as_tuple(std::move(vert)), std::forward_as_tuple(verticesItCount));
+
+			if (emplRes.second)
+			{
+				vertices[verticesItCount++] = (emplRes.first)->first;
+			}
+
+			indices[indicesItCount++] = (emplRes.first)->second;
+		}
+
+		//discard 2 bytes
+		stlStream.seekg(2, std::ios_base::cur);
+
+		//if (stlStream.good() == false)
+		//{
+		//	//error handling here
+		//	//probably handle eof especially -> better error messages
+		//}
+	}
+
+	stlStream.exceptions(origExceptMask);
+
+	return MeshInstance::create
+		(IndexBufferBase::PRIMITIVE_TYPE_TRIANGLES
+		, vertices.get(), verticesItCount
+		, indices.get(), indicesCount);
 }
 
 
