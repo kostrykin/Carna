fa9d23d54ab1bb0c9da91ae8d979cf01c3ee6d9b
Leonid Kostrykin <kostrykin@evoid.de>
2021-06-08 19:43:09 +0200

    adds optional parameters to `linux_build.sh`

--- linux_build.sh
+++ linux_build.sh
@@ -5,11 +5,11 @@ mkdir -p build/make_debug
 mkdir -p build/make_release
 
 cd build/make_debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=ON -DBUILD_TEST=ON -DBUILD_DEMO=ON ../..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=ON -DBUILD_TEST=ON -DBUILD_DEMO=ON $* ../..
 make
 
 cd ../make_release
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=OFF -DBUILD_TEST=ON -DBUILD_DEMO=ON ../..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=OFF -DBUILD_TEST=ON -DBUILD_DEMO=ON $* ../..
 make
 
 echo
