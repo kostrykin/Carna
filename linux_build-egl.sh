be64fc8a0d2a2a86605163eff50d00c2ed3e7cc9
Leonid Kostrykin <void@evoid.de>
2023-12-12 20:53:46 +0100

    Set `BUILD_DOC=OFF` if doxygen is not found

--- linux_build-egl.sh
+++ linux_build-egl.sh
@@ -2,15 +2,21 @@
 
 set -e
 
+if command -v doxygen &> /dev/null; then
+    export BUILD_DOC="ON"
+else    
+    export BUILD_DOC="OFF"
+fi
+
 mkdir -p build/make_debug
 mkdir -p build/make_release
 
 cd build/make_debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=ON -DBUILD_TEST=OFF -DBUILD_DEMO=OFF -DBUILD_EGL=ON $* ../..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=$BUILD_DOC -DBUILD_TEST=OFF -DBUILD_DEMO=OFF -DBUILD_EGL=ON $* ../..
 make
 
 cd ../make_release
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=OFF -DBUILD_TEST=OFF -DBUILD_DEMO=OFF -DBUILD_EGL=ON $* ../..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=$BUILD_DOC -DBUILD_TEST=OFF -DBUILD_DEMO=OFF -DBUILD_EGL=ON $* ../..
 make
 
 echo
