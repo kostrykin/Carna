5116863f1089bdd443799916b7f62462ea0d9a5b
Leonid Kostrykin <void@evoid.de>
2025-04-16 09:55:51 +0200

    Add  env variable

--- linux_build.bash
+++ linux_build.bash
@@ -27,12 +27,14 @@ fi
 echo
 echo "** Ready to install."
 
-if [ "$BUILD" != "only_release" ]; then
-    cd ../make_debug
-    sudo make install
-fi
-
-if [ "$BUILD" != "only_debug" ]; then
-    cd ../make_release
-    sudo make install
+if [ "$INSTALL" != "off" ]; then
+    if [ "$BUILD" != "only_release" ]; then
+        cd ../make_debug
+        sudo make install
+    fi
+    
+    if [ "$BUILD" != "only_debug" ]; then
+        cd ../make_release
+        sudo make install
+    fi
 fi
